a3d67752990ae09ab35eaad704ea1244c419b47f
Vladimir <sapozhnikovl@yandex.ru>
2024-02-04 15:04:53 +0800

    Добавляет реьд разметку первой страницы

--- source/js/script.js
+++ source/js/script.js
@@ -22,3 +22,29 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-scrollbar',
   },
 });
+
+const swiper1 = new Swiper('.swiper2', {
+  // Optional parameters
+  direction: 'horizontal',
+  slidesPerView: 5,
+  loop: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
